3d6797d95640ffcacc65e2620e3291a9f6ea013a
mjrosengrant <mike@rosengrant.me>
2021-08-04 10:22:00 -0400

    fix stuffz

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,13 +3,21 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+container_name='align'
+
+existing=$(docker ps -aq -f name=$container_name)
+if [ -n "$existing" ]; then
+    echo "removing existing container"
+    docker rm -f $existing
+fi
+
 # Create redeploy.sh
 echo "./deploy.sh $*" > redeploy.sh
 chmod +x redeploy.sh
 
 docker run -d \
---name align \
+--name $container_name \
 --restart unless-stopped \
 -e ARGS="$*" \
-align
+$container_name
 
