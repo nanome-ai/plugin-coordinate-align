0f0ef8b759d58be7d2a7778b8a42d12cc7e1ce61
mjrosengrant <mike@rosengrant.me>
2021-08-04 18:07:06 +0000

    Add devcontainer

--- align_plugin/run.py
+++ align_plugin/run.py
@@ -4,7 +4,7 @@ from plugin import AlignPlugin
 if __name__ == "__main__":
     # Information describing the plugin
     name = 'Align Tool'
-    description = "Set complex matrix to be in relation to reference complex."
+    description = 'Override the 3D local coordinate space of a complex to be relative to a reference complex'
     category = 'Align'
     has_advanced = False
     Plugin.setup(name, description, category, has_advanced, AlignPlugin)
