7022074b7c76329958dd5bfda4ef44c67f9eea16
mjrosengrant <mike@rosengrant.me>
2021-08-18 14:06:49 +0000

    Rename Plugin

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from plugin_align_tool.AlignTool import AlignToolPlugin
 
 if __name__ == "__main__":
     # Information describing the plugin
-    name = 'Align Tool'
-    description = 'Override the 3D local coordinate space of a complex to be relative to a reference complex'
+    name = 'Coordinate Align'
+    description = 'Override the 3D coordinates of a complex to be relative to a reference complex'
     category = 'Align'
     has_advanced = False
     Plugin.setup(name, description, category, has_advanced, AlignToolPlugin)
