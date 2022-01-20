8bb8703c901ada66c90f122dbe72fabaeaa3dcdb
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2022-01-20 13:08:53 -0500

    update category (#12)

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     # Information describing the plugin
     name = 'Coordinate Align'
     description = 'Override the 3D coordinates of a complex to be relative to a reference complex'
-    category = 'Align'
+    category = 'Alignment'
     has_advanced = False
     Plugin.setup(name, description, category, has_advanced, AlignToolPlugin)
